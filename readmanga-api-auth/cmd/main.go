c2d6e67e030aa694dbb6f716513c28f1e1cef989
SevenProxy <flapbraga@gmail.com>
2025-05-02 23:16:09 -0300

    edit files

--- readmanga-api-auth/cmd/main.go
+++ readmanga-api-auth/cmd/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"readmanga-api-auth/application"
+	"readmanga-api-auth/config"
+	"readmanga-api-auth/domain"
 	"readmanga-api-auth/infra/database"
+	"readmanga-api-auth/infra/repository"
 	"readmanga-api-auth/infra/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	config.LoadEnv()
+
 	db := database.Connect()
+	db.AutoMigrate(&domain.User{}, &domain.Notification{})
+
+	userRepo := repository.NewUserRepository(db)
+	userApp := application.NewUserApplication(userRepo)
+	
 	server := gin.Default()
 
 	api := server.Group("/api/v1")
 	{
-		routes.RegisterUserRoutes(api)
+		routes.RegisterUserRoutes(api, userApp)
 	}
 
 	server.Run(":3001")
